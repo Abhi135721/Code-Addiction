e154985039105eb25099a000e277fdb036eb3a32
Abhi135721 <abhi.amma18@gmail.com>
2018-02-13 18:25:18 +0530

    Add one to number by abhi

--- interviewBit/Arrays/Add_One_To_Number/Abhi.java
+++ interviewBit/Arrays/Add_One_To_Number/Abhi.java
@@ -3,6 +3,7 @@
     Firstly , sounds simply like adding one to the last index's digit and return the array
 		But , Edge cases are if last digit is 9 ---> it becomes 10 , 
 		So , updated it with 0 and added 1 to preceding digit
+		for cases like 9999 we have to increase array size by 1 to store 10000
 */
 public class Abhi{
     public static void main(String [] args){
@@ -15,9 +16,11 @@ public class Abhi{
 		A = plusOne(A);
 	}
     public int[] plusOne(int[] A) {
+		//checked if last index is 0 if it is 0 add one to it it will be 1 (0 + 1)
         if(A[A.length - 1] == 0 ){
             A[A.length - 1] = 1;
         }
+		//if it is 9 make it as 0 and add 1 to preceding digit
         else if(A[A.length - 1] == 9){
 			A[A.length - 1] = 0;
 			for(int i = A.length - 2 ; i >= 0; i--){
@@ -32,10 +35,12 @@ public class Abhi{
     			}
     		}
     		boolean flag = true;
+			
     		for(int i = 0 ; i < A.length ; i++){
     		    if(A[i] != 0)
     		        flag = false;
     		}
+			// so for 9999 it will become 0000 ,still needed to increase array size add 1 to first index
     		if(flag){
     			int [] B = new int[A.length + 1];
     			B[0] = 1;
@@ -45,9 +50,11 @@ public class Abhi{
     			return B;
     		}
 		}
+		//simply adding one to last digit
 		else{
 		    A[A.length - 1] = A[A.length - 1] + 1;
 		}
+		// if a number is given 00089 we just need to return 90 .... not 00090
 		int count = 0;
 		for(int i = 0 ; i < A.length ; i++){
 		    if(A[i] != 0){
