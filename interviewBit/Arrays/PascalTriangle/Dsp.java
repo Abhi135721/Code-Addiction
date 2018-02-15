b3ecc785529a575a8249763373b55e10d07e3f75
dspbalu <dspbalu038@gmail.com>
2018-02-15 12:56:38 +0530

    update

---
+++ interviewBit/Arrays/PascalTriangle/Dsp.java
@@ -0,0 +1,17 @@
+/*pascal triangle*/
+public class Solution {
+    public ArrayList<ArrayList<Integer>> generate(int A) {
+         ArrayList<ArrayList<Integer>> ans=new ArrayList<ArrayList<Integer>>();
+        for (int i=0;i<A;i++){
+            ArrayList<Integer> arr=new ArrayList<Integer>();
+            for (int j=0;j<=i;j++){
+              if (i==j || j==0)
+                   arr.add(1);
+              else
+                   arr.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
+            }
+            ans.add(arr);
+        }
+        return ans;
+    }
+}
