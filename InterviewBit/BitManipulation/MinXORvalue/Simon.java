f4b04dc5c7372438323fb021ec28ca9c30ca00f0
Simon Simu <simonsimu77@gmail.com>
2018-02-17 21:25:25 +0530

    added BitManipulation

---
+++ InterviewBit/BitManipulation/MinXORvalue/Simon.java
@@ -0,0 +1,17 @@
+/*
+Question:	Given an array of N integers, find the pair of integers in the array which have minimum XOR value. 
+			Report the minimum XOR value.
+			
+Approach:	Sort the array and update the minimum xor value of adjacent elements and update the value
+*/
+
+public class Solution {
+    public int findMinXor(int[] A) {
+        Arrays.sort(A);
+        int min=A[0]^A[1];
+        for(int i=0;i<A.length-1;i++){
+            if(min>(A[i]^A[i+1]))min=A[i]^A[i+1];
+        }
+        return min;
+    }
+}
---
+++ InterviewBit/BitManipulation/SingleNumber/Simon.java
@@ -0,0 +1,15 @@
+/*
+Question:	Given an array of integers, every element appears twice except for one. Find that single one.
+
+Approach:	Perform xor operation to every bit and return the result
+			Input is {A,A,B,B,C}
+			Output:
+			A^A^B^B^C-->0^0^C-->C (since A^A=0)
+*/
+public class Solution {
+    public int singleNumber(final int[] A) {
+        int sum=0;
+        for(int i=0;i<A.length;i++)sum=sum^A[i];
+        return sum;
+    }
+}
