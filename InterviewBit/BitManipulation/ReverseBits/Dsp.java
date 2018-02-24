e099e8b2c505b76f4451f733f1b5004b53a5235b
dspbalu <dspbalu038@gmail.com>
2018-02-24 13:46:00 +0530

    reverse bits

---
+++ InterviewBit/BitManipulation/ReverseBits/Dsp.java
@@ -0,0 +1,24 @@
+/*
+Reverse bits of an 32 bit unsigned integer
+*/
+/*
+Example:
+x = 3,
+
+          00000000000000000000000000000011 
+=>        11000000000000000000000000000000
+return 3221225472
+*/
+
+public class Solution {
+	public long reverse(long a) {
+	    long result = 0;
+        int i=31;
+        while(a>0){
+            result+=(a%2)*Math.pow(2,i);
+            i--;                        
+            a=a/2;
+        }
+        return result;
+	}
+}
