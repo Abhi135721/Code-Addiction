68351df5bedc9b299146c3fb80a56c47f24637fa
dspbalu <dspbalu038@gmail.com>
2018-04-27 20:51:10 +0530

    jumpgame array

---
+++ InterviewBit/DynamicProgramming/JumpGameArray/Dsp.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int canJump(int[] A) {
+        if(A.length==1)
+        return 1;
+        int max=A[0];
+        for(int i=0;i<A.length;i++){
+            if(max<=i && A[i]==0)
+            return 0;
+            if(i+A[i]>max)
+            max=i+A[i];
+            if(max>=A.length-1)
+            return 1;
+        }
+        return 0;
+    }
+}
