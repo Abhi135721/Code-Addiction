23f054e0bd78de820f3ca167fdbe9b69d4e45382
dspbalu <dspbalu038@gmail.com>
2018-04-27 21:09:16 +0530

    LevelOrder

---
+++ InterviewBit/Graphs/LevelOrder/Dsp.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+ public ArrayList < ArrayList < Integer >> levelOrder(TreeNode root) {
+  ArrayList < ArrayList < Integer >> al = new ArrayList < ArrayList < Integer >> ();
+  ArrayList < Integer > nodeValues = new ArrayList < Integer > ();
+  if (root == null)
+   return al;
+
+  LinkedList < TreeNode > current = new LinkedList < TreeNode > ();
+  LinkedList < TreeNode > next = new LinkedList < TreeNode > ();
+  current.add(root);
+
+  while (!current.isEmpty()) {
+   TreeNode node = current.remove();
+
+   if (node.left != null)
+    next.add(node.left);
+   if (node.right != null)
+    next.add(node.right);
+
+   nodeValues.add(node.val);
+   if (current.isEmpty()) {
+    current = next;
+    next = new LinkedList < TreeNode > ();
+    al.add(nodeValues);
+    nodeValues = new ArrayList();
+   }
+  }
+  return al;
+ }
+}
